e581024d4c44b09e61cc4609321f6c087c6e40c0
Rom <roman1199@mail.ru>
2020-06-17 01:57:19 +0700

    master
    Signed-off-by: Rom <roman1199@mail.ru>

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    arrows:true,
+    slidesToShow:1,
+    slidesToscroll:1,
+    speed:1000,
+    autoplay:true,
+    autoplaySpeed:500,
+    infinite: true,
+  });
+
+});
